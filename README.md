83a9d9fe108a4e5b22ae9c9ea2cfc4913dc97c8a
sjovanovic974 <sjovanovic974@gmail.com>
2021-11-19 22:49:00 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # pet-clinic
+
 Spring Boot Pet Clinic
---
+++ src/test/java/sasa/jovanovic/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package sasa.jovanovic.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
