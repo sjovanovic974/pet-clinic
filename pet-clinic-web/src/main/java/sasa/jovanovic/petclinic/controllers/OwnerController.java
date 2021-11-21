797e083021474d393ea3016d80e0de132735ce4e
sjovanovic974 <sjovanovic974@gmail.com>
2021-11-21 19:17:57 +0100

    Adding owners page and controller. Closes #13.

---
+++ pet-clinic-web/src/main/java/sasa/jovanovic/petclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package sasa.jovanovic.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String listOwners() {
+
+        return "owners/index";
+    }
+}
