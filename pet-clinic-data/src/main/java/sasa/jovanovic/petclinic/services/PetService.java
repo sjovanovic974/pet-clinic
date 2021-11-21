676f3a52b5ae8e5ec703998088a3e9e3e12dfa9e
sjovanovic974 <sjovanovic974@gmail.com>
2021-11-21 14:01:34 +0100

    Refactoring to common CRUD interface. Closes #11.

--- pet-clinic-data/src/main/java/sasa/jovanovic/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/sasa/jovanovic/petclinic/services/PetService.java
@@ -2,13 +2,7 @@ package sasa.jovanovic.petclinic.services;
 
 import sasa.jovanovic.petclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
+public interface PetService extends CrudService<Pet, Long>{
 
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
