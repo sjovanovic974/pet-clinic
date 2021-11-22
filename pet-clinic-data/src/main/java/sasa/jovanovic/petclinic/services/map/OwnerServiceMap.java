238b2a85bddb25c6b98083733ddaaca299382e4d
sjovanovic974 <sjovanovic974@gmail.com>
2021-11-22 18:06:30 +0100

    Added Bootstrap for Data. Closes #14.

--- pet-clinic-data/src/main/java/sasa/jovanovic/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/sasa/jovanovic/petclinic/services/map/OwnerServiceMap.java
@@ -1,11 +1,10 @@
 package sasa.jovanovic.petclinic.services.map;
-
 import sasa.jovanovic.petclinic.model.Owner;
-import sasa.jovanovic.petclinic.services.CrudService;
+import sasa.jovanovic.petclinic.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
 
     @Override
     public Set<Owner> findAll() {
@@ -30,5 +29,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public Owner findById(Long id) {
         return super.findById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
     }
 }
--- pet-clinic-data/src/main/java/sasa/jovanovic/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/sasa/jovanovic/petclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package sasa.jovanovic.petclinic.services.map;
 
 import sasa.jovanovic.petclinic.model.Vet;
-import sasa.jovanovic.petclinic.services.CrudService;
+import sasa.jovanovic.petclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
