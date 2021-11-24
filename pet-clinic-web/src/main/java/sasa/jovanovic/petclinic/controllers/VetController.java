89d67417986a349989499a561eb87d368471314c
sjovanovic974 <sjovanovic974@gmail.com>
2021-11-24 13:24:22 +0100

    Fixed broken links. Closes #45

--- pet-clinic-web/src/main/java/sasa/jovanovic/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/sasa/jovanovic/petclinic/controllers/VetController.java
@@ -14,7 +14,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets/index"})
+    @RequestMapping({"/vets", "/vets/index", "/vets.html"})
     public String listVets(Model model) {
 
         model.addAttribute("vets", vetService.findAll());
